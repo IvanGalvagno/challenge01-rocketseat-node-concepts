f81963530bd079b6c7fe61537c4286221fe29a19
igsrc <ivan.galvagno@gmail.com>
2021-08-26 05:38:35 -0300

    Create User

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  const user = users.find(
+    user => user.username === username
+  );
+
+  if (!user) {
+    return response.status(400).json({
+      error: "User doest exists"
+    });
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body;
+  
+  const  checksExistsUserAccount = users.some((user)=> user.username === username);
+
+  if (checksExistsUserAccount) {
+    return response.status(400).json({message: "User already exists"});
+  }
+  
+  users.push({
+    name,
+    username,
+    id: uuidv4(),
+    todos: []
+  });
+  response.status(201).send();
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
